ee28a5afea1e3b5b5622d2a874872222889972e6
manish <manishkr9547@gmail.com>
2025-05-19 18:37:34 +0530

    Initialize server with Express, CORS, and Clerk integration; set up basic API and webhook handling.

---
+++ server/controllers/clerkWebhooks.js
@@ -0,0 +1,60 @@
+import User from "../models/User";
+import { Webhook } from "svix";
+
+const clerkWebhookes = async (req, res) => {
+  try {
+    // Create a svix instance with clerk secret
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+    // Getting Headers
+    const headers = {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    };
+
+    // Verifying Headers
+    await whook.verify(JSON.stringify(req.body), headers);
+
+    // Getting Data from request body
+    const { data, type } = req.body;
+
+    const userData = {
+      _id: data.id,
+      eamil: data.email_addresses[0].email_address,
+      username: data.first_name + " " + data.last_name,
+      image: data.image_url,
+    };
+
+    // Switch Case for different Events
+    switch (type) {
+      case "user.created": {
+        await User.create(userData);
+        break;
+      }
+      case "user.update": {
+        await User.findByIdAndUpdate(data.id, userData);
+        break;
+      }
+      case "user.deleted": {
+        await User.findByIdAndDelete(data.id);
+        break;
+      }
+
+      default:
+        break;
+    }
+    res.json({
+      success: true,
+      message: "Webhook received and processed successfully",
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export default clerkWebhookes;
